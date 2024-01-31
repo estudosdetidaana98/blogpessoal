4bffec590c10baf8a9582f47f799f45e2eb84b2b
Ana Eliza <estudo.ti.ana@gmail.com>
2024-01-31 15:33:44 -0300

    Deploy do Projeto Blog Pessoal

---
+++ src/main/java/com/generation/blogpessoal/model/Postagem.java
@@ -0,0 +1,78 @@
+package com.generation.blogpessoal.model;
+
+import java.time.LocalDateTime;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.*;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_postagens")
+public class Postagem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "O atributo texto é de preenchimento obrigatório")
+    @Size(min = 5, max = 100, message = "O atributo titulo tem no minino 5 caracteres e no máximo 100 caracteres")
+    private String titulo;
+
+    @NotBlank(message = "O atributo texto é de preenchimento obrigatório")
+    @Size(min = 5, max = 100, message = "O atributo titulo tem no minino 5 caracteres e no máximo 100 caracteres")
+    private String texto;
+
+    @UpdateTimestamp
+    private LocalDateTime data;
+
+    @ManyToOne
+    @JsonIgnoreProperties ("postagem")
+    private Tema tema;
+
+    @ManyToOne
+    @JsonIgnoreProperties("postagem")
+    private Usuario usuario;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getTexto() {
+        return texto;
+    }
+
+    public void setTexto(String texto) {
+        this.texto = texto;
+    }
+
+    public LocalDateTime getData() {
+        return data;
+    }
+
+    public void setData(LocalDateTime data) {
+        this.data = data;
+    }
+
+    public Tema getTema() {
+        return tema;
+    }
+
+    public void setTema(Tema tema) {
+        this.tema = tema;
+    }
+}
